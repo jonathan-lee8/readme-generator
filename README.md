4fdf58243e09d6275facfb63f1413cb5a0761fa6
Jonathan Lee <jonathanhlee8@gmail.com>
2022-09-13 22:03:40 -0700

    adding working js

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Professional README Generator Starter Code
+# ReadMe Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Description
+  READMEs are an important aspect of a developer's work as it takes the opportunity to describe and showcase what the project they were working on is really about. A good ReadMe can describe what the project is, where to access it, who worked on it, and etc. There is no one definite way on how to write a ReadMe, but there are definitely ones that can separate the good from the bad. This ReadMe generator will allow users to create fresh readMes without the stress of creating ones from scratch.
+
+  ## Installation
+  This ReadMe generator can be accessed from my current Github repo, which a link will be included below. A user can either clone the repo from a SSH or HTTP key. User will also need to download Node Package Manager in a terminal using “npm init -y” and download the inquirer package using “npm i inquirer@8.2.4.
+
+  ## Usage
+  Once a repo is cloned and packages are installed, the user can access the terminal and initiate “ node index.js” in index.js and this will initiate the question prompts. Users will fill out according to their needs and project basis and will have completed a readMe.
+
+  undefined
+
+  ## Contribution
+  None
+
+  ## Tests
+  None
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/jonathan-lee8  
+  Email: jonathanhlee8@gmail.com
+
+
+  ## Demonstration 
+
+  https://drive.google.com/file/d/1WJ8w5KpSZvCY75jKKhUq1Ewyw-HjZMbv/view
---
+++ index.js
@@ -0,0 +1,138 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./Develop/utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+      {
+          type: 'input',
+          name: 'title',
+          message: "What is your project's title. (Required)",
+          validate: projName => {
+              if(projName) {
+                  return true;
+              } else {
+                  console.log('Please enter the name of your project!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project. (Required)',
+          validate: projDesc => {
+              if(projDesc){
+                  return true;
+              } else{
+                  console.log('Please enter a project description');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'installation',
+          message: 'Enter detailed instructions for how to get the development environment running for your project. (Required)',
+          validate: projInstall => {
+              if(projInstall){
+                  return true;
+              } else{
+                  console.log('Please enter installation instructions for your project');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'Enter any instructions or examples for use of your project. (Required)',
+          validate: projUse => {
+              if(projUse){
+                  return true;
+              } else{
+                  console.log('Please enter useage instructions or examples for your project');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'contribution',
+          message: 'Enter any guidelines for how you would like individuals to contribute to your project'
+      },
+      {
+          type: 'input',
+          name: 'tests',
+          message: 'Enter any tests for your project'
+      },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'Select a license that applies to your project',
+          choices: ['GPL', 'CC--0', 'MIT', 'None']
+      },
+      {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub username. (Required)',
+          validate: ghName => {
+              if (ghName){
+                  return true;
+              }else {
+                  console.log('Please enter the GitHub Link for your project')
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Enter your email. (Required)',
+          validate: email => {
+              if (email){
+                  return true;
+              }else {
+                  console.log('Please enter the GitHub Link for your project')
+                  return false;
+              }
+          }
+      }
+  ];
+
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/README.md', data, err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        return;
+      }
+
+      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: 'File created! Navigate to the "dist" folder to see your README!'
+      });
+    });
+  });
+};
+
+const init = () => {
+  return inquirer.prompt(questions);
+};
+
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
